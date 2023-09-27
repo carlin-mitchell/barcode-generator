d1265a9e54fb43624ff9412486978d01a53a66b3
Carlin Mitchell <carlinmi@umn.edu>
2023-09-27 07:53:25 -0500

    “sync”

--- src/components/QrCodeGenerator/QrCodeTextInput.js
+++ src/components/QrCodeGenerator/QrCodeTextInput.js
@@ -1,14 +1,16 @@
 // COMPONENT IMPORTS
 import { Div, Input } from "../_elements/Elements";
 import { v4 as uuidv4 } from "uuid"
+import { generateQr, getQrTextInput } from "./logic";
+import { getInputValue } from "../../utils";
 
 // LOGIC IMPORTS
 //
 
 // COMPONENT METHODS
-// function someMethod() {
-//   //
-// }
+function handleInput() {
+    generateQr(getInputValue(getQrTextInput()));
+}
 
 export const componentId = uuidv4();
 export const qrTextInputId = `qr-text-input-${componentId}`
@@ -18,7 +20,11 @@ const QrGenerateTextInput = () => {
     const parentElement = Div(
         { innerText: ``, className: `` },
         // add child elements to the array below
-        [Input({ type: "text", id: qrTextInputId })]
+        [Input({
+            type: "text", id: qrTextInputId, oninput() {
+                handleInput()
+            }
+        })]
     );
     return parentElement;
 };
